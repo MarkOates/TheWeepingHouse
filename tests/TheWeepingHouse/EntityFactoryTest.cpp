c35befa81895b56edddf2b09f2f799a9046d2a68
Mark Oates <markoates0@gmail.com>
2022-08-05 14:20:57 -0400

    * Duplicate EntityFactory from AllegroFlare/Prototypes/FixedRoom2D/EntityFactory

---
+++ tests/TheWeepingHouse/EntityFactoryTest.cpp
@@ -0,0 +1,12 @@
+
+#include <gtest/gtest.h>
+
+#include <TheWeepingHouse/EntityFactory.hpp>
+
+
+TEST(TheWeepingHouse_EntityFactoryTest, can_be_created_without_blowing_up)
+{
+   TheWeepingHouse::EntityFactory entity_factory;
+}
+
+
