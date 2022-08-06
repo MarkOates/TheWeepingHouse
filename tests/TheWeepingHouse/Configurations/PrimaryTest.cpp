c3f9bcee00ba4322081a088c165dccd65a40f625
Mark Oates <markoates0@gmail.com>
2022-08-06 14:33:59 -0400

    * Rename #configurations_builder# to #primary_builder#

--- tests/TheWeepingHouse/Configurations/PrimaryTest.cpp
+++ tests/TheWeepingHouse/Configurations/PrimaryTest.cpp
@@ -27,7 +27,7 @@ class TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH
    : public AllegroFlare::Testing::WithAllegroRenderingFixture
 {
 public:
-   TheWeepingHouse::Configurations::Primary configurations_builder;
+   TheWeepingHouse::Configurations::Primary primary_builder;
 
 private:
    virtual void SetUp() override
@@ -42,72 +42,72 @@ private:
       bitmap_bin.set_full_path(PRODUCTION_BITMAPS_FOLDER);
       font_bin.set_full_path(PRODUCTION_FONTS_FOLDER);
 
-      configurations_builder.set_bitmap_bin(&bitmap_bin);
-      configurations_builder.set_font_bin(&font_bin);
-      configurations_builder.set_event_emitter(&event_emitter);
-      configurations_builder.set_entity_collection_helper__this_is_a_hack(&entity_collection_helper__this_is_a_hack);
+      primary_builder.set_bitmap_bin(&bitmap_bin);
+      primary_builder.set_font_bin(&font_bin);
+      primary_builder.set_event_emitter(&event_emitter);
+      primary_builder.set_entity_collection_helper__this_is_a_hack(&entity_collection_helper__this_is_a_hack);
    }
 };
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryTest, can_be_created_without_blowing_up)
 {
-   TheWeepingHouse::Configurations::Primary configurations_builder;
+   TheWeepingHouse::Configurations::Primary primary_builder;
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    build__will_not_blow_up)
 {
-   configurations_builder.build();
+   primary_builder.build();
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    room_exists__will_return_true_if_the_room_exists)
 {
-   configurations_builder.build();
-   EXPECT_EQ(true, configurations_builder.room_exists("front_porch"));
+   primary_builder.build();
+   EXPECT_EQ(true, primary_builder.room_exists("front_porch"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    room_exists__will_return_false_if_the_room_does_not_exist)
 {
-   configurations_builder.build();
-   EXPECT_EQ(false, configurations_builder.room_exists("a-room-that-does-not-exist"));
+   primary_builder.build();
+   EXPECT_EQ(false, primary_builder.room_exists("a-room-that-does-not-exist"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    script_exists__will_return_true_if_the_script_is_present)
 {
-   configurations_builder.build();
-   EXPECT_EQ(true, configurations_builder.script_exists("observe_front_porch_door"));
+   primary_builder.build();
+   EXPECT_EQ(true, primary_builder.script_exists("observe_front_porch_door"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    script_exists__will_return_false_if_the_script_is_not_present)
 {
-   configurations_builder.build();
-   EXPECT_EQ(false, configurations_builder.script_exists("a-script-that-does-not-exist"));
+   primary_builder.build();
+   EXPECT_EQ(false, primary_builder.script_exists("a-script-that-does-not-exist"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    entity_exists__will_return_true_if_the_entity_is_present)
 {
-   configurations_builder.build();
-   EXPECT_EQ(true, configurations_builder.entity_exists("front_porch_bg"));
+   primary_builder.build();
+   EXPECT_EQ(true, primary_builder.entity_exists("front_porch_bg"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    entity_exists__will_return_false_if_the_entity_is_not_present)
 {
-   configurations_builder.build();
-   EXPECT_EQ(false, configurations_builder.entity_exists("an-entity-that-does-not-exist"));
+   primary_builder.build();
+   EXPECT_EQ(false, primary_builder.entity_exists("an-entity-that-does-not-exist"));
 }
 
 
@@ -117,33 +117,33 @@ TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WIT
    AllegroFlare::BitmapBin &bitmap_bin = get_bitmap_bin_ref();
    bitmap_bin.set_full_path(TEST_FIXTURE_BITMAPS_FOLDER);
 
-   configurations_builder.assemble_room("my_test_room_name", "This is a test room");
+   primary_builder.assemble_room("my_test_room_name", "This is a test room");
 
    // creates a room
-   ASSERT_EQ(true, configurations_builder.room_exists("my_test_room_name"));
-   EXPECT_NE(nullptr, configurations_builder.get_room_dictionary()["my_test_room_name"]);
+   ASSERT_EQ(true, primary_builder.room_exists("my_test_room_name"));
+   EXPECT_NE(nullptr, primary_builder.get_room_dictionary()["my_test_room_name"]);
 
    // creates a background
-   ASSERT_EQ(true, configurations_builder.entity_exists("my_test_room_name_bg"));
-   EXPECT_NE(nullptr, configurations_builder.get_entity_dictionary()["my_test_room_name_bg"]);
+   ASSERT_EQ(true, primary_builder.entity_exists("my_test_room_name_bg"));
+   EXPECT_NE(nullptr, primary_builder.get_entity_dictionary()["my_test_room_name_bg"]);
 
    // creates an "observe_..." script for the background
-   ASSERT_EQ(true, configurations_builder.script_exists("observe_my_test_room_name"));
-   ASSERT_EQ(1, configurations_builder.get_script_dictionary()["observe_my_test_room_name"].get_lines().size());
+   ASSERT_EQ(true, primary_builder.script_exists("observe_my_test_room_name"));
+   ASSERT_EQ(1, primary_builder.get_script_dictionary()["observe_my_test_room_name"].get_lines().size());
    EXPECT_EQ(
       "DIALOG: This is a test room",
-      configurations_builder.get_script_dictionary()["observe_my_test_room_name"].get_lines()[0]
+      primary_builder.get_script_dictionary()["observe_my_test_room_name"].get_lines()[0]
    );
 
    // an association exists for the background entity and the room
-   EXPECT_EQ(true, configurations_builder.entity_room_association_exists("my_test_room_name_bg", "my_test_room_name"));
+   EXPECT_EQ(true, primary_builder.entity_room_association_exists("my_test_room_name_bg", "my_test_room_name"));
 }
 
 
 TEST_F(TheWeepingHouse_Configurations_PrimaryWithAllegroRenderingFixtureTest_WITH_ASSEMBLED_configurations_builder,
    the_production_configuration_has_the_expected_elements)
 {
-   AllegroFlare::Prototypes::FixedRoom2D::Configuration configuration = configurations_builder.build();
+   AllegroFlare::Prototypes::FixedRoom2D::Configuration configuration = primary_builder.build();
    EXPECT_EQ(1, configuration.get_room_dictionary().count("front_hallway"));
    //EXPECT_EQ("front_hallway", configuration.get_starting_in_room_identifier());
 }
