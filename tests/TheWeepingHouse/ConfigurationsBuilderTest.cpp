7e5bc6760faee42970e114e162934807bfa7a5d1
Mark Oates <markoates0@gmail.com>
2022-08-06 10:45:25 -0400

    * Add test that #build# will not blow up in ConfigurationsBuilder

--- tests/TheWeepingHouse/ConfigurationsBuilderTest.cpp
+++ tests/TheWeepingHouse/ConfigurationsBuilderTest.cpp
@@ -5,6 +5,14 @@
 
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
+#ifdef _WIN32
+#define TEST_FIXTURE_FONTS_FOLDER "/msys64/home/Mark/Repos/TheWeepingHouse/bin/programs/data/fonts/"
+#define TEST_FIXTURE_BITMAPS_FOLDER "/msys64/home/Mark/Repos/TheWeepingHouse/bin/programs/data/bitmaps/"
+#else
+#define TEST_FIXTURE_FONTS_FOLDER "/Users/markoates/Repos/TheWeepingHouse/bin/programs/data/fonts/"
+#define TEST_FIXTURE_BITMAPS_FOLDER "/Users/markoates/Repos/TheWeepingHouse/bin/programs/data/bitmaps/"
+#endif
+
 
 class TheWeepingHouse_ConfigurationsBuilderTest : public ::testing::Test{};
 
@@ -19,12 +27,15 @@ TEST_F(TheWeepingHouse_ConfigurationsBuilderTest, can_be_created_without_blowing
 }
 
 
-TEST_F(TheWeepingHouse_ConfigurationsBuilderWithAllegroRenderingFixtureTest, DISABLED__build__will_not_blow_up)
+TEST_F(TheWeepingHouse_ConfigurationsBuilderWithAllegroRenderingFixtureTest, build__will_not_blow_up)
 {
-   AllegroFlare::BitmapBin bitmap_bin;
-   AllegroFlare::FontBin font_bin;
+   AllegroFlare::BitmapBin &bitmap_bin = get_bitmap_bin_ref();
+   AllegroFlare::FontBin &font_bin = get_font_bin_ref();
    AllegroFlare::EventEmitter event_emitter;
    AllegroFlare::Prototypes::FixedRoom2D::EntityCollectionHelper entity_collection_helper__this_is_a_hack;
+
+   bitmap_bin.set_full_path(TEST_FIXTURE_BITMAPS_FOLDER);
+   font_bin.set_full_path(TEST_FIXTURE_FONTS_FOLDER);
 
    TheWeepingHouse::ConfigurationsBuilder configurations_builder(
       &bitmap_bin,
