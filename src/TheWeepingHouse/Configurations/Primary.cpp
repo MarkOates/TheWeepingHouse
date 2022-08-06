044c8bee095634569f59957d0d20e25cf3f8999a
Mark Oates <markoates0@gmail.com>
2022-08-06 00:42:41 -0400

    * Add FRONT_PATIO

--- src/TheWeepingHouse/Configurations/Primary.cpp
+++ src/TheWeepingHouse/Configurations/Primary.cpp
@@ -49,6 +49,11 @@ AllegroFlare::Prototypes::FixedRoom2D::Configuration Primary::build(AllegroFlare
          error_message << "Primary" << "::" << "build" << ": error: " << "guard \"entity_collection_helper__this_is_a_hack\" not met";
          throw std::runtime_error(error_message.str());
       }
+   const std::string MAIN_HALLWAY = "main_hallway";
+   const std::string FRONT_PATIO = "front_patio";
+
+   const std::string FRONT_PATIO_DOOR = "front_patio_door";
+
    AllegroFlare::Prototypes::FixedRoom2D::Configuration result;
 
    AllegroFlare::InventoryIndex &inventory_index = result.get_inventory_index_ref();
@@ -81,32 +86,48 @@ AllegroFlare::Prototypes::FixedRoom2D::Configuration Primary::build(AllegroFlare
    room_dictionary = {
       { "front_hall", room_factory.create_room() },
       { "study", room_factory.create_room() },
+      { FRONT_PATIO, room_factory.create_room() },
    };
 
    entity_dictionary = {
+      { "front_patio_bg", entity_factory.create_background("room_2.png", "observe_front_patio") },
       { "front_hall_bg", entity_factory.create_background("room_0.png", "observe_front_hall") },
-      { "main_hall_bg", entity_factory.create_background("room_1.png", "observe_main_hall") },
+      { "main_hallway_bg", entity_factory.create_background("room_1.png", "observe_main_hallway") },
+
       { "door1", entity_factory.create_rectangle_hitspot(1725, 440, 60, 350, "Door 1", "observe_door1") },
       { "door2", entity_factory.create_rectangle_hitspot(115, 440, 60, 350, "Door 2", "observe_door2") },
+      { FRONT_PATIO_DOOR, entity_factory.create_rectangle_hitspot(1725, 440, 60, 350, "Door 2", "observe_front_patio_door") },
       { "wall_art", entity_factory.create_ellipse_hitspot(1150, 450, 60, 60, "Wall Art", "observe_wall_art") },
    };
 
    entity_room_associations = {
       { "front_hall_bg", "front_hall" },
-      { "main_hall_bg", "study" },
+      { "main_hallway_bg", "study" },
+
+      { "front_patio_bg", FRONT_PATIO },
+      { FRONT_PATIO_DOOR, FRONT_PATIO },
+
       { "door1", "front_hall" },
       { "door2", "study" },
       //{ "chair", "front_hall" },
       { "table", "front_hall" },
       { "wall_art", "front_hall" },
       { "keys", "front_hall" },
+      { "main_hallway_bg", MAIN_HALLWAY },
    };
 
    script_dictionary = {
+      { "observe_front_patio_door", AllegroFlare::Prototypes::FixedRoom2D::Script({
+            "DIALOG: Oh wow, it's unlocked.  I'm going inside.",
+            "ENTER_ROOM: front_hall",
+      })},
+      { "observe_front_patio", AllegroFlare::Prototypes::FixedRoom2D::Script({
+            "DIALOG: What a nice place... but it doesn't look very invnting.  I'd better see if I can get inside."
+      })},
       { "observe_front_hall", AllegroFlare::Prototypes::FixedRoom2D::Script({
             "DIALOG: This is a pretty dark room. | It's kinda hard to see anything, to be honest."
       })},
-      { "observe_main_hall", AllegroFlare::Prototypes::FixedRoom2D::Script({
+      { "observe_main_hallway", AllegroFlare::Prototypes::FixedRoom2D::Script({
             "DIALOG: This is a pretty dark room. | It's kinda hard to see anything, to be honest."
       })},
       { "observe_door1", AllegroFlare::Prototypes::FixedRoom2D::Script({
@@ -131,7 +152,7 @@ AllegroFlare::Prototypes::FixedRoom2D::Configuration Primary::build(AllegroFlare
       })},
    };
 
-   starting_in_room_identifier = "front_hall";
+   starting_in_room_identifier = FRONT_PATIO;
     
    return result;
 }
