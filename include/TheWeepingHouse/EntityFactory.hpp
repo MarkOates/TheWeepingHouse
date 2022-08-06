bcbee94d1c939ce38da8e85c77486cb60af74245
Mark Oates <markoates0@gmail.com>
2022-08-05 21:11:24 -0400

    * Add #create_rectangle_hitspot# and #create_ellipse_hitspot# in EntityFactory

--- include/TheWeepingHouse/EntityFactory.hpp
+++ include/TheWeepingHouse/EntityFactory.hpp
@@ -12,12 +12,17 @@ namespace TheWeepingHouse
    {
    private:
       AllegroFlare::BitmapBin* bitmap_bin;
+      std::string hitspot_rectangle_bitmap_identifier;
+      std::string hitspot_circle_bitmap_identifier;
+      bool hide_hitspots;
 
    public:
       EntityFactory(AllegroFlare::BitmapBin* bitmap_bin=nullptr);
       ~EntityFactory();
 
       AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* create_background(std::string bitmap_image_identifier="[unset-bitmap_identifier]", std::string on_interact_script_name="[unset-on_interact_script_name]");
+      AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* create_rectangle_hitspot(float x=1920/2, float y=1080/2, float w=100, float h=100, std::string name="[unset-name]", std::string on_interact_script_name="[unset-on_interact_script_name]");
+      AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* create_ellipse_hitspot(float center_x=1920/2, float center_y=1080/2, float w=100, float h=100, std::string name="[unset-name]", std::string on_interact_script_name="[unset-on_interact_script_name]");
       AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* create_entity(std::string bitmap_image_identifier="[unset-bitmap_identifier]", float x=1920/2, float y=1080/2, float scale=1.0f, std::string name="[unset-name]", std::string on_interact_script_name="[unset-on_interact_script_name]", float align_x=0.5f, float align_y=1.0f, bool hidden=false);
    };
 }
--- src/TheWeepingHouse/EntityFactory.cpp
+++ src/TheWeepingHouse/EntityFactory.cpp
@@ -5,6 +5,10 @@
 #include <sstream>
 #include <stdexcept>
 #include <sstream>
+#include <stdexcept>
+#include <sstream>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace TheWeepingHouse
@@ -13,6 +17,9 @@ namespace TheWeepingHouse
 
 EntityFactory::EntityFactory(AllegroFlare::BitmapBin* bitmap_bin)
    : bitmap_bin(bitmap_bin)
+   , hitspot_rectangle_bitmap_identifier("hitspot_rectangle_1x1.png")
+   , hitspot_circle_bitmap_identifier("hitspot_circle_100x100.png")
+   , hide_hitspots(false)
 {
 }
 
@@ -34,6 +41,62 @@ AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* EntityFactory::create_bac
       bitmap_image_identifier, 0, 0, 1.0, "background", on_interact_script_name, 0, 0
    );
    result->set_cursor_insights_are_hidden(true);
+   return result;
+}
+
+AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* EntityFactory::create_rectangle_hitspot(float x, float y, float w, float h, std::string name, std::string on_interact_script_name)
+{
+   if (!(bitmap_bin))
+      {
+         std::stringstream error_message;
+         error_message << "EntityFactory" << "::" << "create_rectangle_hitspot" << ": error: " << "guard \"bitmap_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   ALLEGRO_BITMAP* bitmap = bitmap_bin->auto_get(hitspot_rectangle_bitmap_identifier);
+
+   AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* result = new
+      AllegroFlare::Prototypes::FixedRoom2D::Entities::Base(bitmap);
+
+   AllegroFlare::Placement2D &placement = result->get_placement_ref();
+   result->set_on_cursor_interact_script_name(on_interact_script_name);
+   result->set_hidden(hide_hitspots);
+
+   placement.position = {x, y};
+   placement.scale = {w, h};
+   placement.align = {0, 0};
+
+   result->set("name", name);
+   result->set("is_hitspot", true);
+
+   return result;
+}
+
+AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* EntityFactory::create_ellipse_hitspot(float center_x, float center_y, float w, float h, std::string name, std::string on_interact_script_name)
+{
+   if (!(bitmap_bin))
+      {
+         std::stringstream error_message;
+         error_message << "EntityFactory" << "::" << "create_ellipse_hitspot" << ": error: " << "guard \"bitmap_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   ALLEGRO_BITMAP* bitmap = bitmap_bin->auto_get(hitspot_circle_bitmap_identifier);
+   int bitmap_natural_width = al_get_bitmap_width(bitmap);
+   int bitmap_natural_height = al_get_bitmap_height(bitmap);
+
+   AllegroFlare::Prototypes::FixedRoom2D::Entities::Base* result = new
+      AllegroFlare::Prototypes::FixedRoom2D::Entities::Base(bitmap);
+
+   AllegroFlare::Placement2D &placement = result->get_placement_ref();
+   result->set_on_cursor_interact_script_name(on_interact_script_name);
+   result->set_hidden(hide_hitspots);
+
+   placement.position = {center_x, center_y};
+   placement.scale = {bitmap_natural_width / w, bitmap_natural_height / h};
+   placement.align = {0.5, 0.5};
+
+   result->set("name", name);
+   result->set("is_hitspot", true);
+
    return result;
 }
 
