f6703062275f669b6559668fc8f85f71812fdaa8
Mark Oates <markoates0@gmail.com>
2022-08-07 12:27:05 -0400

    * Add test fixture tmj file

--- tests/TheWeepingHouse/ConfigurationTMJLoaderTest.cpp
+++ tests/TheWeepingHouse/ConfigurationTMJLoaderTest.cpp
@@ -4,6 +4,10 @@
 #include <TheWeepingHouse/ConfigurationTMJLoader.hpp>
 
 
+#define TEST_FIXTURE_FILENAME "/Users/markoates/Repos/TheWeepingHouse/tests/test_fixtures/main_hallway_elements.tmj"
+
+
+
 TEST(TheWeepingHouse_ConfigurationTMJLoaderTest, can_be_created_without_blowing_up)
 {
    TheWeepingHouse::ConfigurationTMJLoader configuration_tmjloader;
@@ -12,7 +16,7 @@ TEST(TheWeepingHouse_ConfigurationTMJLoaderTest, can_be_created_without_blowing_
 
 TEST(TheWeepingHouse_ConfigurationTMJLoaderTest, load__does_not_blow_up)
 {
-   TheWeepingHouse::ConfigurationTMJLoader configuration_tmjloader;
+   TheWeepingHouse::ConfigurationTMJLoader configuration_tmjloader(TEST_FIXTURE_FILENAME);
    EXPECT_EQ(true, configuration_tmjloader.load());
 }
 
@@ -20,7 +24,7 @@ TEST(TheWeepingHouse_ConfigurationTMJLoaderTest, load__does_not_blow_up)
 TEST(TheWeepingHouse_ConfigurationTMJLoaderTest, objects__contains_the_extracted_objects)
 {
    TheWeepingHouse::ConfigurationTMJLoader configuration_tmjloader;
-   configuration_tmjloader.load();
+   //configuration_tmjloader.load();
    // TODO
    //configuration_tmjloader.get_objects();
    //SUCCEED();
